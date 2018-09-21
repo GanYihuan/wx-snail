6cd9bc8348509f42605a239ff892cc77a48c1e94
GanYihuan <583520052@qq.com>
2018-09-21 19:03:04 +0800

    Card

--- src/pages/books/Book.vue
+++ src/pages/books/Book.vue
@@ -1,11 +1,13 @@
 ﻿<template>
   <div>
     <!-- <TopSwiper :tops='tops'></TopSwiper> -->
-    <!-- <Card v-for='book in books' :key='book.id' :book='book'></Card> -->
-    <!-- <p class='text-footer' v-if='!more'>
+    <Pic v-for='book in books' :key='book.id' :book='book'></Pic>
+    <p class='text-footer' v-if='!more'>
       没有更多数据
-    </p> -->
-    <div v-for='book in books' :key='book.id'>{{book.title}}</div>
+    </p>
+    <!-- <div v-for='book in books' :key='book.id'>{{book.title}}</div> -->
+    <!-- <Pic v-for='book in books' :key='book.id' :book='book'></Pic> -->
+    <!-- <Pic></Pic> -->
   </div>
 </template>
 
@@ -21,30 +23,62 @@
 // 1. page=0 不能显示这条提醒
 // 2. page>0 数据长度<10 停止触底加载
 import { get } from '@/util'
-// import Card from '@/components/Card'
-// import TopSwiper from '@/components/TopSwiper'
+import Pic from '@/components/Pic'
+import TopSwiper from '@/components/TopSwiper'
 
 export default {
-	// components: {
-	// 	Card,
-	// 	TopSwiper
-	// },
+	components: {
+		Pic,
+		TopSwiper
+	},
 	data() {
 		return {
 			books: []
-			// page: 0,
-			// more: true,
-			// tops: []
 		}
-	},
-	mounted() {
-		this.getList()
 	},
 	methods: {
 		async getList(init) {
-			const books = await get('/weapp/booklist')
-			this.books = books.list
+			if (init) {
+				this.page = 0
+				this.more = true
+			}
+			wx.showNavigationBarLoading()
+			const books = await get('/weapp/booklist', { page: this.page })
+			if (books.list.length < 3 && this.page > 0) {
+				this.more = false
+				console.log('没有更多数据', this.more)
+			}
+			if (init) {
+				this.books = books.list
+				wx.stopPullDownRefresh()
+			} else {
+				// 下拉刷新，不能直接覆盖books 而是累加
+				this.books = this.books.concat(books.list)
+			}
+			wx.hideNavigationBarLoading()
+		},
+		/* 获取排行榜 */
+		async getTop() {
+			const tops = await get('/weapp/top')
+			this.tops = tops.list
 		}
+	},
+	onPullDownRefresh() {
+		console.log('下拉')
+		this.getList(true)
+		this.getTop()
+	},
+	onReachBottom() {
+		if (!this.more) {
+			// 没有更多了
+			return false
+		}
+		this.page = this.page + 1
+		this.getList()
+	},
+	mounted() {
+		this.getList(true)
+		this.getTop()
 	}
 }
 </script>
