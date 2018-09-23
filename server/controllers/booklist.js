c5d046522378d4aa153ab2dce708953b227e04ef
GanYihuan <583520052@qq.com>
2018-09-23 23:22:37 +0800

    8-1 图书列表页获取数据

--- server/controllers/booklist.js
+++ server/controllers/booklist.js
@@ -1,6 +1,6 @@
 const { mysql } = require('../qcloud')
 
-// [data](http://localhost:5757/weapp/booklist)
+/* [data](http://localhost:5757/weapp/booklist) */
 module.exports = async ctx => {
   // const books = await mysql('books').select('*')
   // ctx.state.data = {
@@ -24,21 +24,22 @@ module.exports = async ctx => {
 
   const { page, openid } = ctx.request.query
   const size = 10
-  // 添加微信名和头像要链表查询 .join
+  /* 添加微信名和头像要链表查询 .join */
   const mysqlSelect = mysql('books')
     .select('books.*', 'cSessionInfo.user_info')
     .join('cSessionInfo', 'books.openid', 'cSessionInfo.open_id')
     .orderBy('books.id', 'desc')
   let books
   if (openid) {
-    // 根据opid过滤
+    /* 根据 opid 过滤 */
     books = await mysqlSelect.where('books.openid', openid)
   } else {
-    // 全部图书 分页
+    /* 全部图书 分页 */
     books = await mysqlSelect.limit(size).offset(Number(page) * size)
   }
-  // 返回
+  /* 返回 */
   ctx.state.data = {
+    /* list: 查看 data */
     list: books.map(v => {
       const info = JSON.parse(v.user_info)
       return Object.assign({}, v, {
--- src/pages/books/Book.vue
+++ src/pages/books/Book.vue
@@ -9,16 +9,18 @@
 </template>
 
 <script>
-// [data](http://localhost:5757/weapp/booklist)
-// 35条数据
-// 每次加载10条
-// 0页   0-10
-// 1     10-20
-// 2     20-30（5）
-// page 当前第几页
-// 没有更多数据
-// 1. page=0 不能显示这条提醒
-// 2. page>0 数据长度<10 停止触底加载
+/*
+[data](http://localhost:5757/weapp/booklist)
+35条数据
+每次加载10条
+0页   0-10
+1     10-20
+2     20-30（5）
+page 当前第几页
+没有更多数据
+1. page=0 不能显示这条提醒
+2. page>0 数据长度<10 停止触底加载
+*/
 import { get } from '@/util'
 import Pic from '@/components/Pic'
 import TopSwiper from '@/components/TopSwiper'
@@ -40,37 +42,7 @@ export default {
 		this.getList(true)
 		this.getTop()
 	},
-	methods: {
-		async getList(init) {
-			if (init) {
-				this.page = 0
-				this.more = true
-			}
-			// [showNavigationBarLoading](https://developers.weixin.qq.com/miniprogram/dev/api/ui/navigation-bar/wx.showNavigationBarLoading.html)
-			wx.showNavigationBarLoading()
-			const books = await get('/weapp/booklist', { page: this.page })
-			if (books.list.length < 3 && this.page > 0) {
-				this.more = false
-				console.log('没有更多数据', this.more)
-			}
-			if (init) {
-				this.books = books.list
-				// [stopPullDownRefresh](https://developers.weixin.qq.com/miniprogram/dev/api/ui/pull-down-refresh/wx.stopPullDownRefresh.html)
-				wx.stopPullDownRefresh()
-			} else {
-				// 下拉刷新，不能直接覆盖books 而是累加
-				this.books = this.books.concat(books.list)
-			}
-			// [hideNavigationBarLoading](https://developers.weixin.qq.com/miniprogram/dev/api/ui/navigation-bar/wx.hideNavigationBarLoading.html)
-			wx.hideNavigationBarLoading()
-		},
-		/* 获取排行榜 */
-		async getTop() {
-			const tops = await get('/weapp/top')
-			this.tops = tops.list
-		}
-	},
-	// [refresh](https://developers.weixin.qq.com/miniprogram/dev/framework/config.html#%E5%85%A8%E5%B1%80%E9%85%8D%E7%BD%AE)
+	/* [refresh](https://developers.weixin.qq.com/miniprogram/dev/framework/config.html#%E5%85%A8%E5%B1%80%E9%85%8D%E7%BD%AE) */
 	onPullDownRefresh() {
 		console.log('下拉')
 		this.getList(true)
@@ -78,11 +50,42 @@ export default {
 	},
 	onReachBottom() {
 		if (!this.more) {
-			// 没有更多了
+			/* 没有更多了 */
 			return false
 		}
 		this.page = this.page + 1
 		this.getList()
+	},
+	methods: {
+		async getList(init) {
+			if (init) {
+				this.page = 0
+				this.more = true
+			}
+			/* [showNavigationBarLoading](https://developers.weixin.qq.com/miniprogram/dev/api/ui/navigation-bar/wx.showNavigationBarLoading.html) */
+			wx.showNavigationBarLoading()
+			const books = await get('/weapp/booklist', { page: this.page })
+			if (books.list.length < 3 && this.page > 0) {
+				this.more = false
+				console.log('没有更多数据', this.more)
+			}
+			if (init) {
+        /* list: 查看 data */
+				this.books = books.list
+				/* [stopPullDownRefresh](https://developers.weixin.qq.com/miniprogram/dev/api/ui/pull-down-refresh/wx.stopPullDownRefresh.html) */
+				wx.stopPullDownRefresh()
+			} else {
+				/* 下拉刷新，不能直接覆盖books 而是累加 */
+				this.books = this.books.concat(books.list)
+			}
+			/* [hideNavigationBarLoading](https://developers.weixin.qq.com/miniprogram/dev/api/ui/navigation-bar/wx.hideNavigationBarLoading.html) */
+			wx.hideNavigationBarLoading()
+		},
+		/* 获取排行榜 */
+		async getTop() {
+			const tops = await get('/weapp/top')
+			this.tops = tops.list
+		}
 	}
 }
 </script>
