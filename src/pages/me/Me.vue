43335445b230c6ec771ec38f34567ff30fe9d377
GanYihuan <583520052@qq.com>
2018-10-18 10:03:26 +0800

    format

--- src/pages/me/Me.vue
+++ src/pages/me/Me.vue
@@ -9,7 +9,15 @@
     <button class='btn' v-if='userInfo.openId' @click='scanBook'>
       添加图书
     </button>
-		<button class='btn' v-else @click='login'>
+		<!-- <button class='btn' v-else @click='login'>
+      点击登录
+    </button> -->
+    <button
+      v-else
+      open-type="getUserInfo"
+      lang="zh_CN"
+      @click='doLogin'
+    >
       点击登录
     </button>
   </div>
@@ -64,26 +72,65 @@ export default {
 			})
 			showModal('添加成功', `${res.title}添加成功`)
 		},
+		doLogin() {
+			/* [qcloud 获取用户信息 wafer2-client-sdk](https://github.com/tencentyun/wafer-client-sdk/) */
+			/* [getStorageSync 获取缓存数据](https://developers.weixin.qq.com/miniprogram/dev/api/storage/wx.getStorageSync.html) */
+			let user = wx.getStorageSync('userInfo')
+			if (!user) {
+				if (user) {
+					// 第二次登录
+					// 或者本地已经有登录态
+					// 可使用本函数更新登录态
+					qcloud.loginWithCode({
+						success: res => {
+							console.log(res)
+              /* [setStorageSync 数据缓存](https://developers.weixin.qq.com/miniprogram/dev/api/storage/wx.setStorageSync.html) */
+              wx.setStorageSync('userInfo', res)
+              this.userInfo = res
+              showSuccess('第二次登录成功')
+						},
+						fail: err => {
+							console.log('第二次登录失败', err)
+						}
+					})
+				} else {
+          // 首次登录
+          qcloud.setLoginUrl(config.loginUrl)
+					qcloud.login({
+						success: res => {
+              console.log(res)
+              /* [setStorageSync 数据缓存](https://developers.weixin.qq.com/miniprogram/dev/api/storage/wx.setStorageSync.html) */
+              wx.setStorageSync('userInfo', res)
+              this.userInfo = res
+              showSuccess('登录成功')
+						},
+						fail: err => {
+							console.log('登录失败', err)
+						}
+					})
+				}
+			}
+		},
 		login() {
-      /* [qcloud 获取用户信息 wafer2-client-sdk](https://github.com/tencentyun/wafer-client-sdk/) */
-      /* [getStorageSync 获取缓存数据](https://developers.weixin.qq.com/miniprogram/dev/api/storage/wx.getStorageSync.html) */
-      let user = wx.getStorageSync('userInfo')
+			/* [qcloud 获取用户信息 wafer2-client-sdk](https://github.com/tencentyun/wafer-client-sdk/) */
+			/* [getStorageSync 获取缓存数据](https://developers.weixin.qq.com/miniprogram/dev/api/storage/wx.getStorageSync.html) */
+			let user = wx.getStorageSync('userInfo')
 			if (!user) {
 				qcloud.setLoginUrl(config.loginUrl)
 				qcloud.login({
 					success: userInfo => {
 						console.log('登录成功', userInfo)
 						// showSuccess('登录成功')
-            /* openId 隐藏在 weapp/user 里面 */
-            /* 有 openId 但是没返回, 采取下面部分的写法才能获取 */
-            /* 如果希望小程序的网络请求包含会话，登录之后使用 request 方法进行网络请求即可 */
+						/* openId 隐藏在 weapp/user 里面 */
+						/* 有 openId 但是没返回, 采取下面部分的写法才能获取 */
+						/* 如果希望小程序的网络请求包含会话，登录之后使用 request 方法进行网络请求即可 */
 						qcloud.request({
 							/* 想要使用 optionId 要请求 server/routes/index.js/用户信息接口 */
 							url: config.userUrl,
 							login: true,
 							success: userRes => {
-                console.log(userRes)
-                /* [setStorageSync 数据缓存](https://developers.weixin.qq.com/miniprogram/dev/api/storage/wx.setStorageSync.html) */
+								console.log(userRes)
+								/* [setStorageSync 数据缓存](https://developers.weixin.qq.com/miniprogram/dev/api/storage/wx.setStorageSync.html) */
 								wx.setStorageSync('userInfo', userRes.data.data)
 								this.userInfo = userRes.data.data
 								showSuccess('登录成功')
